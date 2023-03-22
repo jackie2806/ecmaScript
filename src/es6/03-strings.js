b31b2476a0c976626494194e83665ebffb87e939
jackie2806 <jaishackeline@gmail.com>
2023-03-22 18:04:50 -0500

    template literals

---
+++ src/es6/03-strings.js
@@ -0,0 +1,14 @@
+const hello = 'Hello';
+const world = 'World';
+let epicPhrase1 = hello + ' '+ world + '.'
+//Template literals con comillas francesas
+let epicPhrase2 = `${hello} ${world}`;
+
+//Multi-line strings
+
+let lorem = 'Esto es un string \n' + 'esto es otra línea';
+
+let lorem2 = `Esta es una frase épica
+la continuación de esa frase épica
+`;
+console.log(lorem)
