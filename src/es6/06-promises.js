7cd8c399dc1570a36da03b460fbc535bbb683f9d
jackie2806 <jaishackeline@gmail.com>
2023-03-28 15:52:26 -0500

    comentando código

--- src/es6/06-promises.js
+++ src/es6/06-promises.js
@@ -12,3 +12,7 @@ anotherFunction()
     .then(response => console.log(response)) //no colocar punto y coma después del .then
    // .then(response => console.log(response)) Se puede colocar más métodos .then()
     .catch(err => console.log(err))
+
+    /* *.then() ** es un método de la promesa que retorna anotherFunction(),
+     no es “algo llamado then” y si bien es una ‘palabra reservada'. 
+     Es un método, y por ser un método se “llama” o “ejecuta” y recibe parámetros, en este caso un callback (na función como argumento) */
